7bbc7314d50b080528001ca6a4fc32a496deb38c
Jesus Morfin <jmorfin@csumb.edu>
2023-04-17 16:55:59 -0700

    Fixed commit Error/ Added Missile Spawn
    Once again I have committed the files wrong so I have to update again sigh. I also added Missile Spawner.

--- Seasons Defense/Assets/Scripts/Enemy.cs
+++ Seasons Defense/Assets/Scripts/Enemy.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public GameObject enemyMissilePrefab;
+    public GameObject smartBombPrefab;
     
     //Delay how fast the missiles Spawn
     public float delayMissileSpawn = 5f;
 
+    private float yValue;
 
-    
+
 
 
     void Start()
     {
-        
+        yValue = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
         
         StartCoroutine(spawnEnemyMissile());
     }
@@ -23,7 +25,7 @@ public class Enemy : MonoBehaviour
     private IEnumerator spawnEnemyMissile()
     {
         Instantiate(enemyMissilePrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
-        
+        //Instantiate(smartBombPrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
     
         
         yield return new WaitForSeconds(delayMissileSpawn);
