94b8802488d1d0f28018aafdd177bbeff42ba7c3
Miguel A. Francisco <migufran4594@gmail.com>
2023-04-17 13:05:29 -0700

    Update TowerController.cs script
    Instead of doing the shooting in the update function, I decided to make it into another method called Shoot().
    
    This method will be called from the tower decider script once it picks what tower should shoot.

--- Seasons Defense/Assets/Scripts/TowerController.cs
+++ Seasons Defense/Assets/Scripts/TowerController.cs
@@ -4,24 +4,21 @@ public class TowerController : MonoBehaviour
 {
     public GameObject missilePrefab;
     public float offset;
+    
+    public void Fire() {
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = transform.position.z;
+        GameObject missileInstance = Instantiate(missilePrefab, new Vector3(transform.position.x, transform.position.y + offset, 0), Quaternion.identity);
+      
+        // Give the missile the attributes it needs to move towarsd its target position
+        Missile missile = missileInstance.GetComponent<Missile>();
+
+        if (missile != null)
         {
-            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            worldPosition.z = transform.position.z;
-            GameObject missileInstance = Instantiate(missilePrefab, new Vector3(transform.position.x, transform.position.y + offset, 0), Quaternion.identity);
-
-            // Give the missile the attributes it needs to move towarsd its target position
-            Missile missile = missileInstance.GetComponent<Missile>();
-
-            if (missile != null)
-            {
-                missile.speed = 5;
-                missile.target = worldPosition;
-            }
+            missile.speed = 5;
+            missile.target = worldPosition;
         }
     }
 }
