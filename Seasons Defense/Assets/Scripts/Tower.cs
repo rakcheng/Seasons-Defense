69dcc895e64912a7b4fd32e8a2cf57593d1fd9ca
Jesus Morfin <jmorfin@csumb.edu>
2023-04-21 15:00:47 -0700

    Added Targeting to Tower and Cities
    Added Targeting to Tower and Cities and maybe fixed the merge Problem

--- Seasons Defense/Assets/Scripts/Tower.cs
+++ Seasons Defense/Assets/Scripts/Tower.cs
@@ -4,14 +4,21 @@ public class Tower : MonoBehaviour
 {
     public GameObject missilePrefab;
     public float offset;
+    
+    private int missileCount = 5;
     private Vector3 _towerPosition;
-
+    
     private void Start()
     {
         _towerPosition = new Vector3(transform.position.x, transform.position.y + offset, 0);
     }
 
     public void Fire() {
+
+        // a tower will only fire if their missile count is greater than 0
+        if (missileCount == 0) return;
+
+        missileCount--;
 
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
@@ -25,5 +32,17 @@ public class Tower : MonoBehaviour
         {
             missile.target = worldPosition;
         }
+    }
+
+
+    // Methods to set or get the missile count
+    public int getMissileCount()
+    {
+        return this.missileCount;
+    }
+
+    public void ReloadTower(int amount)
+    {
+        this.missileCount += amount;
     }
 }
