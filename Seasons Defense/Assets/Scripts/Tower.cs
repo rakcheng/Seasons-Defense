a78af6310d3027b836a21530bbc9760fcca3bf01
Josh <joshualomelin@gmail.com>
2023-04-21 15:35:12 -0700

    Added basic debug UI for ammo count
    Added a quick and dirty UI panel to the tower prefab that displays the current ammo count dynamically.

--- Seasons Defense/Assets/Scripts/Tower.cs
+++ Seasons Defense/Assets/Scripts/Tower.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class Tower : MonoBehaviour
 {
+    public TextMeshProUGUI ammoText;
+    
     public GameObject missilePrefab;
     public float offset;
     
@@ -11,6 +14,7 @@ public class Tower : MonoBehaviour
     private void Start()
     {
         _towerPosition = new Vector3(transform.position.x, transform.position.y + offset, 0);
+        UpdateAmmoCount();
     }
 
     public void Fire() {
@@ -32,6 +36,8 @@ public class Tower : MonoBehaviour
         {
             missile.target = worldPosition;
         }
+        
+        UpdateAmmoCount();
     }
 
 
@@ -44,5 +50,10 @@ public class Tower : MonoBehaviour
     public void ReloadTower(int amount)
     {
         this.missileCount += amount;
+    }
+
+    private void UpdateAmmoCount()
+    {
+        ammoText.SetText("Ammo: " + missileCount);
     }
 }
