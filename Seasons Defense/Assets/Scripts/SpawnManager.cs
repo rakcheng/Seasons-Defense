9b790a17f4ab7a7cdd1c1746ca64d04dd57da27d
Jesus Morfin <jmorfin@csumb.edu>
2023-04-21 19:10:45 -0700

    Removed Start from Spawn Manager and changed it to Awake
    Removed Start from Spawn Manager and changed it to Awake

--- Seasons Defense/Assets/Scripts/SpawnManager.cs
+++ Seasons Defense/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,9 @@ public  class SpawnManager : MonoBehaviour
     private static Vector3[] _transformPositions;
 
     // Start is called before the first frame update
-    void Start()
+    
+    void Awake()
+
     {
         InitializePositions();
     }
