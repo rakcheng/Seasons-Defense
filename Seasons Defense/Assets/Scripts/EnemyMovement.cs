f3b0e448a31303f04202d3c684ddf2e242b7130e
Jesus Morfin <jmorfin@csumb.edu>
2023-04-23 20:42:54 -0700

    Made changes to the enemy movement  by Making them Despawn after the first collision
    Made changes to the enemy movement  by Making them Despawn after the first collision

--- Seasons Defense/Assets/Scripts/EnemyMovement.cs
+++ Seasons Defense/Assets/Scripts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,24 @@ public class EnemyMovement : MonoBehaviour
 {
     //The speed of the enemies
     public float speed = 5f;
-    public float rightPadding = 11f;
-    public float leftPadding = -11f;
+    public float rightPadding = 35f;
+    public float leftPadding = -35f;
     //To be able to switch direction from left to right or right to left
     private bool _moveRight = true;
+
+    void Start()
+    {
+        //If the switchMovement is true it will move right
+        if (transform.position.x < 0)
+        {
+            _moveRight = true;
+        }
+        //If the switchMovement is false it will move left
+        else
+        {
+            _moveRight = false;
+        }
+    }
 
     void Update()
     {
@@ -27,12 +42,17 @@ public class EnemyMovement : MonoBehaviour
         //If Postion x is greater than or equal to 11 it will change the switchMovement to false
         if (transform.position.x >= rightPadding)
         {
-            _moveRight = false;
+            LevelManager.EnemiesCount--;
+            LevelManager.EnemyDestroyed();
+            Destroy(gameObject);
         }
         //If Postion x is less than or equal to negative 11 it will change the switchMovement to true
         if (transform.position.x <= leftPadding)
         {
-            _moveRight = true;
+           
+            LevelManager.EnemiesCount--;
+            LevelManager.EnemyDestroyed();
+            Destroy(gameObject);
         }
     }
     
