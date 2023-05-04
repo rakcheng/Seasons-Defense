3f20b78fca1ee67fb771ba4cc3cc5d88f0bdbe7f
Jesus Morfin <jmorfin@csumb.edu>
2023-05-03 20:29:51 -0700

    Update City.cs
    Updated City script

--- Seasons Defense/Assets/Scripts/City.cs
+++ Seasons Defense/Assets/Scripts/City.cs
@@ -7,27 +7,27 @@ public class City : MonoBehaviour
 {
     public Animator cityAnimator;
     public GameObject cityDeathParticles;
-    private bool _cityDestoyed;
+
     // todo: add particle effects and/or animation for this
 
     public void Start()
     {
-        _cityDestoyed = false;
+
     }
 
     public void BeingDestroyed()
     {
-        if (_cityDestoyed) return;
-
         GetComponent<Collider>().enabled = false;
-        _cityDestoyed = true;
-        
+
         LevelManager.Instance.citiesCount--;
         LevelManager.Instance.BuildingDestroyed();
         
         cityAnimator.enabled = true;
         GameObject effect = Instantiate(cityDeathParticles, transform.position, Quaternion.identity);
         Destroy(effect, 2f);
+        
+        Destroy(gameObject, 2);
+        
         
     }
 }
