1628cec61deb7a4b9ac54d5f524ca9af2215b235
Jesus Morfin <jmorfin@csumb.edu>
2023-05-03 20:24:49 -0700

    Cannon/city animation and effects
    I added cannon and city animation with there own particle system.

--- Seasons Defense/Assets/Scripts/City.cs
+++ Seasons Defense/Assets/Scripts/City.cs
@@ -1,14 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class City : MonoBehaviour
 {
+    public Animator cityAnimator;
+    public GameObject cityDeathParticles;
+    private bool _cityDestoyed;
     // todo: add particle effects and/or animation for this
+
+    public void Start()
+    {
+        _cityDestoyed = false;
+    }
+
     public void BeingDestroyed()
     {
+        if (_cityDestoyed) return;
+
+        GetComponent<Collider>().enabled = false;
+        _cityDestoyed = true;
+        
         LevelManager.Instance.citiesCount--;
         LevelManager.Instance.BuildingDestroyed();
-        Destroy(gameObject);
+        
+        cityAnimator.enabled = true;
+        GameObject effect = Instantiate(cityDeathParticles, transform.position, Quaternion.identity);
+        Destroy(effect, 2f);
+        
     }
 }
